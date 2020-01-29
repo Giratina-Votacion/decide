43de0090d363d4c6d9bf6bf4b0ca22743b0ef0d8
Minuke <josminrey@alum.us.es>
2020-01-29 20:05:59 +0100

    Commit correspondiente a la tarea #44

--- decide/decide/views.py
+++ decide/decide/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from voting.models import Voting, Question
+from census.models import Census
+from base.models import Auth
+from django.http.response import HttpResponse
 
 def home(request):
     return render(request, 'voting/home.html')
@@ -8,4 +11,15 @@ def home(request):
 def listaVotaciones(request):
     votaciones=Voting.objects.all()
     cuestiones=Question.objects.all()
-    return render(request, 'voting/vistaVotaciones.html', {'votaciones':votaciones, 'cuestiones':cuestiones})
+    censo=Census.objects.all()
+    autoridades=Auth.objects.all()
+    return render(request, 'voting/vistaVotaciones.html', {'votaciones':votaciones, 'cuestiones':cuestiones, 'censo':censo, 'autoridades':autoridades})
+
+def listaUsuarios(request, mundo):
+    votaciones=Voting.objects.all()
+    cuestiones=Question.objects.all()
+    censo=Census.objects.all()
+    autoridades=Auth.objects.all()
+    documento= mundo
+    return render(request,'voting/vistaUsuarios.html',{'votaciones':votaciones, 'cuestiones':cuestiones, 'censo':censo, 'autoridades':autoridades,'documento':documento})
+    
