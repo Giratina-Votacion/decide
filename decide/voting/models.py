d8f34c82c7747e62c98d01fdf43ec78dbbaafdce
Minuke <josminrey@alum.us.es>
2020-01-30 11:30:34 +0100

    Commit correspondiente a la tarea #50

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -40,7 +40,6 @@ class Voting(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True, null=True)
     public = models.BooleanField(default = False)
-    public = models.BooleanField(default = False)
     question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE)
 
     start_date = models.DateTimeField(blank=True, null=True)
