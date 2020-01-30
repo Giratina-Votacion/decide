00daf983162fe9d5522c98b65d7106bea888f0a2
Minuke <josminrey@alum.us.es>
2020-01-30 14:03:08 +0100

    Commit correspondiente a la tarea #55

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -14,6 +14,11 @@ from mixnet.mixcrypt import ElGamal
 from mixnet.mixcrypt import MixCrypt
 from mixnet.models import Auth
 from voting.models import Voting, Question, QuestionOption
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
+import unittest 
+from selenium import webdriver
+import time 
 
 
 class VotingTestCase(BaseTestCase):
@@ -364,7 +369,7 @@ class votacionFalse(TestCase):
             super().tearDown()
             self.voting = None
 
-    def test_store_census(self):
+    def test_store_voting(self):
             self.assertEqual(Voting.objects.count(), 1)
 
 class votacionTrue(TestCase):
@@ -383,5 +388,95 @@ class votacionTrue(TestCase):
             super().tearDown()
             self.voting = None
 
-    def test_store_census(self):
+    def test_store_voting(self):
             self.assertEqual(Voting.objects.count(), 1)
+
+# TEST DE VISTAS
+
+""" class TestSignUpCorrect(unittest.TestCase):
+
+    def setUp(self):
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Firefox(firefox_options=firefox_options)
+        
+    def test_signUpCorrect(self):
+        self.driver.get("http://localhost:8000/admin/login/?next=/admin/")
+        username = self.driver.find_element_by_id('id_username')
+        username.clear
+        username.send_keys("Minuke")
+        password = self.driver.find_element_by_id('id_password')
+        password.clear
+        password.send_keys("decidegc")
+        self.driver.find_element_by_xpath("//input[@value='Iniciar sesión']").click()
+        self.assertTrue(len(self.driver.find_elements_by_id('user-tools'))>0)
+
+    def tearDown(self):
+        self.driver.quit
+
+if __name__ == '__main__':
+    unittest.main()
+
+class TestSignUpIncorrect(unittest.TestCase):
+
+    def setUp(self):
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Firefox(firefox_options=firefox_options)
+        
+    def test_signUpInorrect(self):
+        self.driver.get("http://localhost:8000/admin/login/?next=/admin/")
+        username = self.driver.find_element_by_id('id_username')
+        username.clear
+        username.send_keys("xxxxxxxx")
+        password = self.driver.find_element_by_id('id_password')
+        password.clear
+        password.send_keys("xxxxxxxx")
+        self.driver.find_element_by_xpath("//input[@value='Iniciar sesión']").click()
+        self.assertTrue(len(self.driver.find_elements_by_id('user-tools'))==0)
+
+    def tearDown(self):
+        self.driver.quit
+
+if __name__ == '__main__':
+    unittest.main()
+
+class TestVotacionSiNo(unittest.TestCase):
+
+    def setUp(self):
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Firefox(firefox_options=firefox_options)
+
+    def test_signUpCorrect(self):
+        self.driver.get("http://localhost:8000/admin/login/?next=/admin/")
+        username = self.driver.find_element_by_id('id_username')
+        username.clear
+        username.send_keys("Minuke")
+        password = self.driver.find_element_by_id('id_password')
+        password.clear
+        password.send_keys("decidegc")
+        self.driver.find_element_by_xpath("//input[@value='Iniciar sesión']").click()
+        self.driver.find_element_by_xpath("//a[contains(@href, '/admin/voting/question/add/')]").click()
+        pregunta = self.driver.find_element_by_id('id_desc')
+        pregunta.clear
+        pregunta.send_keys("¿Estas satisfecho con tu nota del M3?")
+        opcion0 =  self.driver.find_element_by_id('id_options-0-number')
+        opcion0.clear
+        opcion0.send_keys(1)
+        opcion1 =  self.driver.find_element_by_id('id_options-1-number')
+        opcion1.clear
+        opcion1.send_keys(2)
+        self.driver.find_element_by_xpath('//input[@id="id_options-0-yes"]').click()
+        self.driver.find_element_by_xpath('//input[@id="id_options-1-no"]').click()
+        self.driver.find_element_by_xpath('//input[@name="_save"]').click()
+        self.assertTrue(len(self.driver.find_elements_by_class_name('success'))>0)
+
+    def tearDown(self):
+        self.driver.quit
+
+if __name__ == '__main__':
+    unittest.main() """
